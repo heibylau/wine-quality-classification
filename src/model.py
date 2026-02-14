a511112af0469b4e428735b239de50e20af6877b
Heiby <117860288+heibylau@users.noreply.github.com>
2026-02-14 14:46:06 -0700

    added functions for running models

---
+++ src/model.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import numpy as np
+import sklearn.model_selection as skm
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.model_selection import cross_val_score, KFold
+from sklearn.metrics import accuracy_score, classification_report
+
+'''
+This file contains functions that run the Logistic regression, 
+Support Vector Machine, and Linear Discriminant Analysis.
+'''
+
+def _run_cv(model, x_train, y_train):
+    '''
+    Helper function to run a 5-fold cross validation.
+    '''
+    kfold = KFold(5, shuffle=True, random_state=42)
+    cv_score = cross_val_score(model, x_train, y_train, cv=kfold)
+    return cv_score
+
+def run_logistic_regression(x_train, x_test, y_train, y_test):
+    '''
+    Trains and evaluates a logistic regression classifier.
+    '''
+    model = LogisticRegression(max_iter=1000)
+    cv_score = _run_cv(model, x_train, y_train)
+    model.fit(x_train, y_train)
+    predictions = model.predict(x_test)
+    accuracy = accuracy_score(y_test, predictions)
+    return model, accuracy, cv_score
+
+def run_svm(x_train, x_test, y_train, y_test, kernel='linear', c=1, gamma=1, degree=2):
+    '''
+    Trains and evaluates a support vector classifier.
+    '''
+    model = SVC()
+    if (kernel == 'poly'):
+        model = SVC(kernel=kernel, C=c, gamma=gamma, degree=degree)
+    elif (kernel == 'linear' or kernel == 'rbf'):
+        model = SVC(kernel=kernel, C=c, gamma=gamma)
+    cv_score = _run_cv(model, x_train, y_train)
+    model.fit(x_train, y_train)
+    predictions = model.predict(x_test)
+    accuracy = accuracy_score(y_test, predictions)
+    return model, accuracy, cv_score
+
+def run_lda(x_train, x_test, y_train, y_test, n_components=1):
+    '''
+    Trains and evaluates an LDA classifier.
+    '''
+    model = LinearDiscriminantAnalysis(n_components=n_components)
+    cv_score = _run_cv(model, x_train, y_train)
+    model.fit(x_train, y_train)
+    predictions = model.predict(x_test)
+    accuracy = accuracy_score(y_test, predictions)
+    return model, accuracy, cv_score
