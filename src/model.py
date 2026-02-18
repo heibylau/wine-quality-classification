0c16cdcda62771c5b4ce09d064d8d9d93370224b
Heiby <117860288+heibylau@users.noreply.github.com>
2026-02-18 13:59:15 -0700

    run more models

--- src/model.py
+++ src/model.py
@@ -1,10 +1,10 @@
-import pandas as pd
-import numpy as np
-import sklearn.model_selection as skm
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.model_selection import cross_val_score, KFold
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV, cross_val_score, KFold
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score, classification_report
 
 '''
@@ -12,12 +12,12 @@ This file contains functions that run the Logistic regression,
 Support Vector Machine, and Linear Discriminant Analysis.
 '''
 
-def _run_cv(model, x_train, y_train):
+def _run_cv(model, X_train, y_train):
     '''
     Helper function to run a 5-fold cross validation.
     '''
     kfold = KFold(5, shuffle=True, random_state=42)
-    cv_score = cross_val_score(model, x_train, y_train, cv=kfold)
+    cv_score = cross_val_score(model, X_train, y_train, cv=kfold)
     return cv_score
 
 def _print_report(y_test, y_pred, cv_score):
@@ -34,17 +34,17 @@ def _print_full_report(y_test, y_pred):
     report = classification_report(y_test, y_pred, target_names=["0=Poor", "1=Good"])
     print(report)
 
-def run_logistic_regression(x_train, x_test, y_train, y_test):
+def run_logistic_regression(X_train, X_test, y_train, y_test):
     '''
     Trains and evaluates a logistic regression classifier.
     '''
     model = LogisticRegression(max_iter=1000)
-    cv_score = _run_cv(model, x_train, y_train)
-    model.fit(x_train, y_train)
-    y_pred = model.predict(x_test)
+    cv_score = _run_cv(model, X_train, y_train)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
     _print_report(y_test, y_pred, cv_score)
 
-def run_svm(x_train, x_test, y_train, y_test, kernel='linear', c=1, gamma=1, degree=2):
+def run_svm(X_train, X_test, y_train, y_test, kernel='linear', c=1, gamma=1, degree=2):
     '''
     Trains and evaluates a support vector classifier.
     '''
@@ -53,17 +53,111 @@ def run_svm(x_train, x_test, y_train, y_test, kernel='linear', c=1, gamma=1, deg
         model = SVC(kernel=kernel, C=c, gamma=gamma, degree=degree)
     elif (kernel == 'linear' or kernel == 'rbf'):
         model = SVC(kernel=kernel, C=c, gamma=gamma)
-    cv_score = _run_cv(model, x_train, y_train)
-    model.fit(x_train, y_train)
-    y_pred = model.predict(x_test)
+    cv_score = _run_cv(model, X_train, y_train)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
     _print_report(y_test, y_pred, cv_score)
 
-def run_lda(x_train, x_test, y_train, y_test, n_components=1):
+def run_lda(X_train, X_test, y_train, y_test, n_components=1):
     '''
     Trains and evaluates an LDA classifier.
     '''
     model = LinearDiscriminantAnalysis(n_components=n_components)
-    cv_score = _run_cv(model, x_train, y_train)
-    model.fit(x_train, y_train)
-    y_pred = model.predict(x_test)
+    cv_score = _run_cv(model, X_train, y_train)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
     _print_report(y_test, y_pred, cv_score)
+
+def tune_svm(X_train, y_train, kernel, c, gamma, degree=2):
+    '''
+    Tunes the hyperparameters for a support vector classifier.
+    The hyperparameters are: cost, gamma, and degree (if kernel is polynomial)
+    '''
+    pipeline = Pipeline([
+        ('scaler', StandardScaler()),
+        ('svm', SVC(kernel=kernel))
+    ])
+    if kernel == 'rbf' or kernel == 'linear':
+        param_dist = {
+            'svm__C': c,
+            'svm__gamma': gamma
+        }
+        search = RandomizedSearchCV(
+            pipeline,
+            param_distributions=param_dist,
+            n_iter=30,
+            cv=5,
+            verbose=1,
+            n_jobs=-1,
+            random_state=42
+        )
+    elif kernel == 'poly':
+        param_dist = {
+            'svm__C': c,
+            'svm__gamma': gamma,
+            'svm__degree': degree,
+        }
+        search = RandomizedSearchCV(
+            pipeline,
+            param_distributions=param_dist,
+            n_iter=15,
+            cv=5,
+            verbose=2,
+            n_jobs=-1,
+            random_state=42
+        )
+
+    search.fit(X_train, y_train)
+    print(f"Best parameters for {kernel} kernel:", search.best_params_)
+    return search.best_estimator_
+
+def tune_lda(X_train, y_train):
+    '''
+    Tuning the LDA classifier.
+    Solver: 'svd', 'lsqr', 'eigen'
+    Shrinkage: None, 'auto
+    '''
+    pipeline = Pipeline([
+        ('scaler', StandardScaler()),
+        ('lda', LinearDiscriminantAnalysis())
+    ])
+
+    params = [
+        {'lda__solver': ['svd']},  # svd cannot use shrinkage
+        {'lda__solver': ['lsqr', 'eigen'], 'lda__shrinkage': [None, 'auto']}
+    ]
+
+    grid = GridSearchCV(pipeline, param_grid=params, cv=5, scoring='accuracy')
+
+    grid.fit(X_train, y_train)
+    print(f"Best parameters: {grid.best_params_}")
+    return grid.best_estimator_
+
+def build_classification_tree(X_train, y_train):
+    '''
+    Builds and tunes a classification tree.
+    '''
+    param_dist = {
+        'max_depth': [None, 2, 3, 4, 5, 6, 7, 8, 10],
+        'min_samples_split': [2, 5, 10, 15],
+        'min_samples_leaf': [1, 2, 4, 6],
+        'criterion': ['gini', 'entropy']
+    }
+
+    tree = DecisionTreeClassifier(random_state=42)
+
+    search = RandomizedSearchCV(
+        tree,
+        param_distributions=param_dist,
+        n_iter=20,
+        cv=5,
+        scoring='accuracy',
+        verbose=1,
+        n_jobs=-1,
+        random_state=42
+    )
+
+    search.fit(X_train, y_train)
+    print(f"Best parameters: {search.best_params_}")
+    print(f"Best CV score: {search.best_score_:.2f}")
+    return search.best_estimator_
